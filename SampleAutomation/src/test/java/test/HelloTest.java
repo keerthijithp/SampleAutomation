6d4139c53f318048f61a1798c2ed060ee9e75878
SOORAJ <SOORAJ@DESKTOP-B5NHARB>
2022-10-10 15:18:51 +0530

    First Commit

---
+++ SampleAutomation/src/test/java/test/HelloTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void testMethod() {
+		System.out.println("Hello am in HelloTest");
+	}
+
+}
---
+++ SampleAutomation/src/test/java/test/TestHelloWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+@Test
+public void secondTest() {
+	System.out.println("I am in second method");
+}
+}
